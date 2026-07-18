6d66b8bf13dc2c5c0c28888152a22f3931321ae0
Imane <ramadaneimane8@gmail.com>
2026-07-18 15:32:56 +0100

    ajout,suppression  des patients

--- src/Api/Patient.js
+++ src/Api/Patient.js
@@ -2,10 +2,11 @@ import connexion from "./Connexion";
 
 export const patientApi = {
   register: (data) => connexion.post("/patients", data),
-  getPaginated: (page = 0, size = 20) =>
-    connexion.get("/patient/pagines", { params: { page, size } }),
+  getPaginated: (page = 0, size = 5) =>
+    connexion.get("/patients/patientPagines", { params: { page, size } }),
   getAllPatients: () => connexion.get("/patients"),
   getById: (id) => connexion.get(`/patients/${id}`),
   update: (id, date) => connexion.put(`/patients/${id}`, data),
   delete: (id) => connexion.delete(`/patients/${id}`),
 };
+ 
